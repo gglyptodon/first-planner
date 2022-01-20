3c31fb1b4ef5c56a1b2a670663df6246fee68292
Janina Mass <janina.mass@gmail.com>
2022-01-20 15:20:43 +0100

    insert csv data into sqlite3 db

--- first-planner/src/bin/enter.rs
+++ first-planner/src/bin/enter.rs
@@ -1,46 +1,90 @@
 use clap::Parser;
-use serde::Deserialize;
+use first_planner::workout::Workout;
+use rusqlite::{Connection, Result};
+use serde_rusqlite::*;
 use std::error::Error;
 use std::io;
 use std::process;
-use first_planner::workout::Workout;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
     tag: String,
+
+    #[clap(short, long, default_value = "wo.db")]
+    db: String,
 }
 
-fn read_csv() -> Result<(), Box<dyn Error>> {
-    // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.headers() {
-        let header = result;
-        println!("{:?}", header);
-    }
-    for result in rdr.deserialize() {
-        let record: Workout = result?;
-        println!("{:?}", record);
-    }
+fn create_db(connection: &Connection) -> Result<(), rusqlite::Error> {
+    connection.execute(
+        "create table if not exists workouts (
+             tag text not null,
+             week integer not null,
+             workout_type text not null,
+             pace_category text not null,
+             description text
+         )",
+        [],
+    )?;
 
-    for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
-        let record = result?;
-
-        println!("{:?}", record);
+    Ok(())
+}
+fn insert(workouts: &[Workout], connection: &Connection) -> rusqlite::Result<()> {
+    for w in workouts {
+        connection.execute(
+            "INSERT INTO workouts (tag, week, workout_type, pace_category, description )
+            VALUES ( 'test', :week, :workout_type, :pace_category, :description)
+           ",
+            to_params_named_with_fields(
+                &w,
+                &["week", "workout_type", "pace_category", "description"],
+            )
+            .unwrap()
+            .to_slice()
+            .as_slice(),
+        )?;
     }
     Ok(())
 }
-fn main() {
+
+fn read_csv() -> Result<Vec<Workout>, Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut results: Vec<Workout> = Vec::new();
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    if let Ok(result) = rdr.headers() {
+        println!("{:?}", result);
+    }
+
+    for result in rdr.deserialize() {
+        let record: Workout = result?;
+        println!("w {:?}", record);
+        results.push(record.clone());
+        println!("v{:?}", &results);
+    }
+    Ok(results)
+}
+
+fn main() -> rusqlite::Result<()> {
     let args = Args::parse();
+    let workouts: Vec<Workout>;
 
     println!("Tag: {}", args.tag);
     println!("Reading csv from stdin.");
 
-    if let Err(err) = read_csv() {
-        println!("error: {}", err);
+    if let Ok(x) = read_csv() {
+        println!("read: {:?}", x);
+        workouts = x;
+    } else {
+        println!("error reading csv");
         process::exit(1);
     }
+
+    //create table if it doesn't exist
+    let conn = Connection::open(args.db)?;
+    create_db(&conn)?;
+    insert(&workouts, &conn)?;
+    println!("inserted");
+    Ok(())
 }
