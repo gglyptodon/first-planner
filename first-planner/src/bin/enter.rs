51590c3cfa2b0fa61aa3761fd3203f02313464eb
Janina Mass <janina.mass@gmail.com>
2022-01-20 12:08:26 +0100

    cleanup

--- first-planner/src/bin/enter.rs
+++ first-planner/src/bin/enter.rs
@@ -44,22 +44,3 @@ fn main() {
         process::exit(1);
     }
 }
-
-#[derive(Debug, Deserialize)]
-struct Record {
-    city: String,
-    region: String,
-    country: String,
-    population: Option<u64>,
-}
-
-fn example() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.deserialize() {
-        // Notice that we need to provide a type hint for automatic
-        // deserialization.
-        let record: Record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
