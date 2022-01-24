f0f526bb5376424d549c1be74edc12a5dc194262
jmass <janina.mass@hhu.de>
2022-01-24 10:32:00 +0100

    modular refactor

---
+++ first-planner/first-server/src/bin/enter.rs
@@ -0,0 +1,149 @@
+use clap::Parser;
+use first_shared::workout::{PaceCategory, TaggedWorkout, Workout, WorkoutType};
+use rusqlite::{params, Connection, Result};
+use serde_rusqlite::*;
+use std::error::Error;
+use std::io;
+use std::process;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    tag: String,
+
+    #[clap(short, long, default_value = "./app.db")]
+    db: String,
+
+    #[clap(short, long, takes_value = false)]
+    update: bool,
+}
+
+fn create_db(connection: &Connection) -> Result<(), rusqlite::Error> {
+    connection.execute(
+        "create table if not exists workouts (
+             tag text not null,
+             week integer not null,
+             workout_type text not null,
+             pace_category text not null,
+             description text,
+             distance text,
+            UNIQUE(tag, week, workout_type)
+         )",
+        [],
+    )?;
+
+    Ok(())
+}
+fn update(workouts: &[Workout], connection: &Connection, tag: &str) -> rusqlite::Result<()> {
+    for w in workouts {
+        let mut stmt = connection.prepare(
+            "UPDATE workouts SET description = ?, distance =?  WHERE tag = ? AND week = ? AND workout_type = ?"
+        )?;
+
+        stmt.execute(params![
+            w.description,
+            w.distance,
+            tag,
+            w.week,
+            w.workout_type.to_string()
+        ])?;
+    }
+
+    Ok(())
+}
+
+fn insert(workouts: &[Workout], connection: &Connection, tag: &str) -> rusqlite::Result<()> {
+    for w in workouts {
+        let tagged = TaggedWorkout::new(w.clone(), tag.to_string());
+        connection.execute(
+            "INSERT INTO workouts (tag, week, workout_type, pace_category, description )
+                      VALUES ( :tag, :week, :workout_type, :pace_category, :description)",
+            to_params_named_with_fields(
+                &tagged,
+                &[
+                    "tag",
+                    "week",
+                    "workout_type",
+                    "pace_category",
+                    "description",
+                ],
+            )
+            .unwrap()
+            .to_slice()
+            .as_slice(),
+        )?;
+    }
+    Ok(())
+}
+
+fn read_csv() -> Result<Vec<Workout>, Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut results: Vec<Workout> = Vec::new();
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    for result in rdr.deserialize() {
+        let record: Workout = result?;
+        results.push(record.clone());
+    }
+    Ok(results)
+}
+
+fn main() -> rusqlite::Result<()> {
+    let args = Args::parse();
+    let workouts: Vec<Workout>;
+
+    println!("Info: tag: {}", args.tag);
+    println!("Info: db: {}", args.db);
+    println!("Info: update: {}", args.update);
+    println!("Info: Reading csv from stdin."); //todo
+
+    if let Ok(x) = read_csv() {
+        workouts = x;
+    } else {
+        println!("error reading csv");
+        process::exit(1);
+    }
+
+    //create table if it doesn't exist
+    let conn = Connection::open(args.db)?;
+
+    create_db(&conn)?;
+    get_all_rows(&conn)?;
+    if args.update {
+        update(&workouts, &conn, &args.tag)?;
+    } else if let Err(e) = insert(&workouts, &conn, &args.tag) {
+        println!("Error {}. Maybe try --update", e);
+        process::exit(1);
+    }
+
+    Ok(())
+}
+
+fn get_all_rows(connection: &Connection) -> rusqlite::Result<()> {
+    let mut stmt = connection.prepare(
+        "SELECT tag, week, description,workout_type,pace_category, distance FROM workouts",
+    )?;
+    let workout_iter = stmt
+        .query_map([], |row| {
+            let pace_cat: String = row.get(3).unwrap();
+            let wo_type: String = row.get(2).unwrap();
+            let tag: String = row.get(0).unwrap();
+
+            Ok((
+                tag,
+                Workout {
+                    week: row.get(1)?,
+                    description: row.get(2)?,
+                    pace_category: PaceCategory::new(pace_cat), //todo
+                    workout_type: WorkoutType::new(wo_type),
+                    distance: row.get(4)?,
+                },
+            ))
+        })
+        .unwrap();
+    for w in workout_iter {
+        println!("Found  {:?}", w.unwrap());
+    }
+    Ok(())
+}
---
+++ first-planner/first-shared/src/workout.rs
@@ -0,0 +1,202 @@
+use chrono::Duration;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
+pub enum WorkoutType {
+    Interval,
+    Tempo,
+    Long,
+}
+
+impl WorkoutType {
+    pub fn new(input: String) -> Self {
+        match input.as_str() {
+            "Interval" => WorkoutType::Interval,
+            "Tempo" => WorkoutType::Tempo,
+            "Long" => WorkoutType::Long,
+            _ => WorkoutType::Long,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Workout {
+    pub week: i32,
+    pub description: String,
+    pub workout_type: WorkoutType,
+    pub pace_category: PaceCategory,
+    pub distance: String, //eg 2k
+}
+
+impl fmt::Display for Workout {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "w{} : {} {}", self.week, self.description, self.distance,)
+    }
+}
+
+impl fmt::Display for WorkoutType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            WorkoutType::Interval => {
+                write!(f, "Interval")
+            }
+            WorkoutType::Tempo => {
+                write!(f, "Tempo")
+            }
+            WorkoutType::Long => {
+                write!(f, "Long")
+            }
+        }
+    }
+}
+
+impl Workout {
+    pub fn new(
+        week: i32,
+        description: String,
+        workout_type: WorkoutType,
+        pace_category: PaceCategory,
+        distance: String,
+    ) -> Self {
+        Self {
+            week,
+            description,
+            workout_type,
+            pace_category,
+            distance,
+        }
+    }
+    pub fn show_with_pace(&self, d: Duration) -> String {
+        format!(
+            "w{} : {} {} @ {}",
+            self.week,
+            self.description,
+            self.distance,
+            calc_paces(&d, &self.pace_category).print_pace_formatted()
+        )
+    }
+}
+
+pub fn calc_paces(base_pace: &chrono::Duration, ps: &PaceCategory) -> chrono::Duration {
+    let mut result = base_pace.checked_add(&chrono::Duration::seconds(0));
+    match ps {
+        PaceCategory::M400 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(37));
+        }
+        PaceCategory::M600 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(34));
+        }
+        PaceCategory::M800 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(31));
+        }
+        PaceCategory::K1 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(29));
+        }
+        PaceCategory::M1200 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(27));
+        }
+        PaceCategory::M1600 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(24));
+        }
+        PaceCategory::K2 => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(21));
+        }
+        PaceCategory::ShortTempo => {
+            result = base_pace.checked_sub(&chrono::Duration::seconds(0));
+        }
+        PaceCategory::MidTempo => {
+            result = base_pace.checked_add(&chrono::Duration::seconds(9));
+        }
+        PaceCategory::LongTempo => {
+            result = base_pace.checked_add(&chrono::Duration::seconds(18));
+        }
+        PaceCategory::Long => {
+            result = base_pace.checked_add(&chrono::Duration::seconds(30)); //todo
+        }
+    }
+    if let Some(x) = result {
+        x
+    } else {
+        *base_pace // .clone()
+    }
+}
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum PaceCategory {
+    M400,
+    M600,
+    M800,
+    K1,
+    M1200,
+    M1600,
+    K2,
+    ShortTempo,
+    MidTempo,
+    LongTempo,
+    Long,
+}
+
+impl PaceCategory {
+    pub fn new(input: String) -> Self {
+        match input.as_str() {
+            "M400" => PaceCategory::M400,
+            "M600" => PaceCategory::M600,
+            "M800" => PaceCategory::M800,
+            "K1" => PaceCategory::K1,
+            "M1200" => PaceCategory::M1200,
+            "M1600" => PaceCategory::M1600,
+            "K2" => PaceCategory::K2,
+            "ShortTempo" => PaceCategory::ShortTempo,
+            "MidTempo" => PaceCategory::MidTempo,
+            "LongTempo" => PaceCategory::LongTempo,
+            "Long" => PaceCategory::Long,
+            _ => PaceCategory::K1,
+        }
+    }
+}
+
+pub trait PacePrinter {
+    fn print_pace_formatted(&self) -> String;
+}
+impl PacePrinter for chrono::Duration {
+    fn print_pace_formatted(&self) -> String {
+        if self.num_minutes() > 60 {
+            "hours...".to_string()
+        } else {
+            let minutes_to_display = self.num_minutes();
+            let dur_submin: chrono::Duration;
+            if let Some(x) = self.checked_sub(&Duration::minutes(self.num_minutes())) {
+                dur_submin = x
+            } else {
+                dur_submin = chrono::Duration::seconds(0)
+            }
+
+            let seconds_to_display = dur_submin.num_seconds();
+            format!("{:02}:{:02}min/km", minutes_to_display, seconds_to_display)
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TaggedWorkout {
+    pub week: i32,
+    pub description: String,
+    pub workout_type: WorkoutType,
+    pub pace_category: PaceCategory,
+    pub distance: String,
+    pub tag: String,
+}
+impl TaggedWorkout {
+    //todo: refactor
+    pub fn new(workout: Workout, tag: String) -> Self {
+        TaggedWorkout {
+            week: workout.week,
+            description: workout.description,
+            workout_type: workout.workout_type,
+            pace_category: workout.pace_category,
+            distance: workout.distance,
+            tag,
+        }
+    }
+}
